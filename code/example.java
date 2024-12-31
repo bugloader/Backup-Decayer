fe374ed1ab0796854387d5d4070d13c2f3a4c1a1
bugloader <36098023+bugloader@users.noreply.github.com>
2024-12-30 18:44:40 -0600

    initialized

---
+++ code/example.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class example {
+    //0.5h delete, 0.5h new, 18 months
+    static int totalTimes=365*2*24;
+    static int timesPerDel=16;
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= totalTimes; i++) {
+            list.add(i);
+            if(i%timesPerDel==0){
+                ArrayList<Integer> newList = new ArrayList<>();
+                newList.add(list.get(0));
+                double threshold=(i-list.get(0))* 3 /4.0;
+                for (int j = 1; j < list.size()-1; j++) {
+                    if(i-list.get(j)<=threshold){
+                        newList.add(list.get(j));
+                        threshold=threshold* 3/4.0;
+                    }
+                }
+                newList.add(list.get(list.size()-1));
+                list=newList;
+                for (int j = 0; j < list.size(); j++) {
+                    System.out.print((i-list.get(j))+" ");
+                }
+                System.out.println("\n"+list.size()+"\n");
+            }
+        }
+    }
+}
---
+++ code/generator.java
@@ -0,0 +1,18 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class generator {
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 1000; i++) {
+            Thread.sleep(3000);
+            try (PrintWriter writer = new PrintWriter(i+".zip")) {
+                writer.println(i);
+                writer.println("This is another line.");
+                System.out.println("Content written to file.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
